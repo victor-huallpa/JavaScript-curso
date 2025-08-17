5f9e97c06b111d4f641f46c67257bb14e056b76c
victor-huallpa <vech.600@gmail.com>
2025-08-16 21:07:59 -0500

    Planteo de ejercicios de clases y objetos avanzados

---
+++ intermedio_js/06-advanced-objects-classes-exercises.js
@@ -0,0 +1,20 @@
+
+// 1. Agregega una función al prototipo de un objeto
+
+// 2. Crea un objeto que herede de otro
+
+// 3. Define un método de instancia en un objeto
+
+// 4. Haz uso de get y set en un objeto
+
+// 5. Utiliza la operación assign en un objeto
+
+// 6. Crea una clase abstracta
+
+// 7. Utiliza polimorfismo en dos clases diferentes
+
+// 8. Implementa un Mixin
+
+// 9. Crea un Singleton
+
+// 10. Desarrolla un Proxy
