3708be5255c6eddf027c624cab1c6b281aa6348b
victor-huallpa <vech.600@gmail.com>
2025-08-22 17:39:49 -0500

    ejercicios resueltos de debugging

--- intermedio_js/18-debugging-exercises.js
+++ intermedio_js/18-debugging-exercises.js
@@ -1,3 +1,16 @@
 // 1. Crea un código con un error lógico y usa VS Code para encontrarlo
+function exaple(){
+    setTimeout(()=>{
+        console.log("hello")
+    }, 1000)
+}
+exaple()
 
 // 2. Experimenta con breakpoints y observa cómo cambia el flujo de ejecución
+
+function exaple(){
+    setTimeout(()=>{
+        console.log("hello")
+    }, "1000")
+}
+exaple()
